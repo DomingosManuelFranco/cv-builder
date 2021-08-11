b9d9be817f646fb08db44eba83430523bc0c5eba
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-08-11 11:08:27 +0100

    finishing up

---
+++ src/components/personal/Personal.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+
+
+class Personal extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Personal
